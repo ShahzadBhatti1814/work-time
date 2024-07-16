b2358427b58eb0c6c501cb839a8b4943bfebc841
ShahzadBhatti1814 <sb393310@gmail.com>
2024-07-15 23:30:52 -0700

    my third repo

---
+++ body.js
@@ -0,0 +1,7 @@
+function calculate(){
+    const num1 = document.querySelector("#num1").value;
+    const num2 = document.querySelector("#num2").value;
+  
+    const result = +num1 + +num2
+    console.log(result)
+}
